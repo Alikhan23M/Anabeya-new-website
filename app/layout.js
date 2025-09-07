a5800bb1c40f3f0f263c72fe35db27ad3b06f6ca
alikhan-devs <alikhandevs@gmail.com>
2025-09-07 13:16:24 +0500

    Add offline support and pwa

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Providers from "./provider.js";
-import Navbar from "@/components/Navbar";
-import BottomNav from "@/components/BottomNav";
-import Footer from "@/components/Footer";
 import LayoutComponent from "@/components/LayoutComponent";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +15,16 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
+       <head>
+        {/* PWA manifest + theme color */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#e4b68a" />
+
+        {/* iOS support */}
+        <link rel="apple-touch-icon" href="/images/anabeya.jpg" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body className={inter.className}>
         <Providers>
           <LayoutComponent>{children}</LayoutComponent>
--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
+const withPWA = require("next-pwa")({
+  dest: "public",                // where service worker & manifest are generated
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // disable in dev
+  cacheOnFrontEndNav: true,
+  fallbacks: {
+    document: "/offline.html",   // fallback page when offline
+  },
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-   eslint: {
+const nextConfig = withPWA({
+  eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com','https://i.ytimg.com'],
+    domains: [
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "i.ytimg.com",
+    ],
     remotePatterns: [
       {
         protocol: "https",
@@ -15,6 +30,6 @@ const nextConfig = {
   experimental: {
     esmExternals: false,
   },
-};
+});
 
 module.exports = nextConfig;
